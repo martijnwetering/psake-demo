3a1b9d561fcb9c71984172704130072ca55819b0
Martijn van de Wetering <vandewetering.m@gmail.com>
2015-11-03 15:15:15 +0100

    Initial commit

---
+++ build.ps1
@@ -0,0 +1,16 @@
+﻿cls
+
+Remove-Module [p]sake
+
+$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | Select-Object -last 1
+
+Import-Module $psakeModule
+
+Invoke-psake -buildFile .\Build\default.ps1 -taskList Test `
+-properties @{ 
+	"buildConfiguration" = "Release"
+	"buildPlatform" = "Any CPU" } `
+-parameters @{"solutionFile" = "..\psake.sln" } `
+-framework 4.5.2
+
+Write-Host "Build exit code: $lastExitCode"
