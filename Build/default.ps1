898fe5e1d23f9510c016579298258969af588123
Martijn van de Wetering <vandewetering.m@gmail.com>
2015-11-03 15:36:59 +0100

    Added build project to source control.

---
+++ Build/default.ps1
@@ -0,0 +1,51 @@
+﻿properties {
+	$testMessage = "Executed test"
+
+	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
+	$outputDirectory = "$solutionDirectory\.build"
+	$temporaryOutputDirectory = "$outputDirectory\temp"
+	$buildConfiguration = "Release"
+	$buildPlatform = "Any CPU"
+}
+
+FormatTaskName "`r`n`r`n---------- Executing {0} Task ----------"
+
+task default -depends Test
+
+task Init -description "Initialises the build by removing previous artifacts
+	and creating output directories" -requiredVariables outputDirectory, temporaryOutputDirectory {
+	
+	Assert -conditionToCheck ("Debug", "Release" -contains $buildConfiguration) -failureMessage "Invalid build configuration '$buildConfiguration' `
+		valid values are 'Debug' or 'Release'"
+
+	Assert -conditionToCheck ("x86", "x64", "Any CPU" -contains $buildPlatform) `
+		-failureMessage "Invalid build platform '$buildPlatform'. Valid values are 'x86', 'x64' or 'Any CPU'"
+
+	# Remove previous build results
+	if (Test-Path $outputDirectory)
+	{
+		Write-Host "Removing output directory located at $outputDirectory"
+		Remove-Item $outputDirectory -Force -Recurse
+	}
+
+	Write-Host "Creating output directory located at ..\.build"
+	New-Item $outputDirectory -ItemType Directory | Out-Null
+		
+	Write-Host "Creating temporary directory located at $temporaryOutputDirectory"
+	New-Item $temporaryOutputDirectory -ItemType Directory | Out-Null	
+}
+
+task Compile -depends Init {
+	Write-Host "Building solution $solutionFile"
+	Exec {
+		msbuild $solutionFile "/p:Configuration=$buildConfiguration;Platform=$buildPlatform;OutDir=$temporaryOutputDirectory"
+	}
+}
+
+task Clean -description "Remove temporary files" {
+	Write-Host "Executed Clean!"
+}
+
+task Test -depends Compile, Clean {
+	Write-Host $testMessage
+}
