0459e37fa5157621586037ecab03b5ad5998b87a
Martijn van de Wetering <vandewetering.m@gmail.com>
2015-11-11 11:38:23 +0100

    xUnit test project toegevoegd en twee test gemaakt (1 in mstest en 1 in xunit)

---
+++ WebApplication.Tests/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+
+namespace WebApplication.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About()
+        {
+            var sut = new HomeController();
+            var result = sut.About() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ WebApplication.xUnit.Tests/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using WebApplication.Controllers;
+using Xunit;
+
+namespace WebApplication.xUnit.Tests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Contact()
+        {
+            var sut = new HomeController();
+            var result = sut.Contact() as ViewResult;
+
+            Assert.NotNull(result);
+        }
+    }
+}
