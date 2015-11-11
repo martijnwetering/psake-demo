656f5df211ec99b78c22c6e2d3d439a949382874
Martijn van de Wetering <vandewetering.m@gmail.com>
2015-11-11 16:18:04 +0100

    Added NUnit test executing to Build project

--- Build/default.ps1
+++ Build/default.ps1
@@ -1,9 +1,19 @@
-﻿properties {
+﻿Include ".\helpers.ps1"
+
+properties {
 	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
 	$outputDirectory = "$solutionDirectory\.build"
 	$temporaryOutputDirectory = "$outputDirectory\temp"
+
+	$publishedNUnitTestDirectory = "$temporaryOutputDirectory\_PublishedNUnitTests"
+	$testResultsDirectory = "$outputDirectory\TestResults"
+	$NUnitTestResultsDirectory = "$testResultsDirectory\NUnit"
+	
 	$buildConfiguration = "Release"
 	$buildPlatform = "Any CPU"
+
+	$packagesPath = "$solutionDirectory\packages"
+	$NUnitExe = (Find-PackagePath $packagesPath "NUnit.Runners") + "\Tools\nunit-console-x86.exe"
 }
 
 FormatTaskName "`r`n`r`n---------- Executing {0} Task ----------"
@@ -19,6 +29,10 @@ task Init -description "Initialises the build by removing previous artifacts
 	Assert -conditionToCheck ("x86", "x64", "Any CPU" -contains $buildPlatform) `
 		-failureMessage "Invalid build platform '$buildPlatform'. Valid values are 'x86', 'x64' or 'Any CPU'"
 
+	# Check that all tools are available
+	Write-Host "Checking that all required tools are available"
+	Assert (Test-Path $NUnitExe) "NUnit Console could not be found"
+		
 	# Remove previous build results
 	if (Test-Path $outputDirectory)
 	{
@@ -51,6 +65,31 @@ task Clean -description "Remove temporary files" {
 	Write-Host "Executed Clean!"
 }
 
-task Test -depends Compile, Clean {
+task Test -depends Clean, Compile, TestNUnit, TestXUnit, TestMSTest -description "Run unit tests" {
 	Write-Host "Executing tests!"
+}
+
+task TestNUnit -depends Compile -precondition { return Test-Path $publishedNUnitTestDirectory } -description "Runs NUnit tests" {
+	$projects = Get-ChildItem $publishedNUnitTestDirectory
+
+	# Create the test results directory if needed
+	if (!(Test-Path $NUnitTestResultsDirectory))
+	{
+		Write-Host "Creating test results directory lacated at $NUnitTestResultsDirectory"
+		New-Item -ItemType Directory -Path $NUnitTestResultsDirectory
+	}
+
+	# Get the list of test DLLs
+	$testAssemblies = $projects | ForEach-Object { $_.FullName + "\bin\" + $_.Name + ".dll" }
+	$testAssembliesParameter = [string]::Join(" ", $testAssemblies)
+
+	Exec { &$NUnitExe $testAssembliesParameter /xml:$NUnitTestResultsDirectory\NUnit.xml /nologo /noshadow }
+}
+
+task TestXUnit -depends Compile -description "Runs xUnit tests" {
+
+}
+
+task TestMSTest -depends Compile -description "Runs MSTest tests" {
+
 }
---
+++ WebApplication.NUnit.Tests/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using WebApplication.Controllers;
+
+namespace WebApplication.NUnit.Tests
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            var sut = new HomeController();
+            var result = sut.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
